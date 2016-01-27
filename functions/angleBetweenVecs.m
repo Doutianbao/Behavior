7b2e753c773992ace80ab16f53c3944fdf7d36e1
Avinash Pujala <avinashpujala@gmail.com>
2016-01-26 22:44:19 -0500

    Improvements to fish orientation tracking

--- functions/angleBetweenVecs.m
+++ functions/angleBetweenVecs.m
@@ -1,5 +1,7 @@
 
 function theta = angleBetweenVecs(v1,v2)
+% angleBetweenVecs - Given 2 vectors, finds the angle between them
+% theta = angleBetweenVecs(v1,v2)
 theta = acos(dot(v1,v2)/(norm(v1)*norm(v2))) * (180/pi);
 
 end

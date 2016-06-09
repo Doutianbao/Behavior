4d1707d83abbb64349b8e6d5b0af8ca242c17770
Avinash Pujala <avinashpujala@gmail.com>
2016-06-09 19:16:52 -0400

    GetMidlines_cl --> GetMidlines

--- functions/GetMidlines.m
+++ functions/GetMidlines.m
@@ -22,11 +22,13 @@ function midlineInds = GetMidlines(IM,varargin)
 %   where T is the number of images in the series and L is number of line
 %   indices. L = sum(lineLens).
 %
-% Avinash Pujala, HHMI, 2016
+% Avinash Pujala, Koyama lab/HHMI, 2016
 
 heights =  [18 16 14 10];
-dTh = 2;
+dTh = 1;
 imgExt = 'jpg';
+ref =[];
+
 if nargin == 1
     if ischar(IM) && isdir(IM)
         IM  = ReadImgSequence(IM,imgExt);
@@ -34,13 +36,22 @@ if nargin == 1
     fishPos = GetFishPos(ProcessImages(IM),50);
 elseif nargin == 2
     fishPos = varargin{1};
-elseif nargin ==3
+elseif nargin == 3
     fishPos = varargin{1};
     heights = varargin{2};
-elseif nargin ==4
+elseif nargin > 3
     fishPos = varargin{1};
     heights = varargin{2};
     imgExt = varargin{3};
+end
+
+for jj =  3:numel(varargin)
+    if isstr(varargin{jj})
+    switch lower(varargin{jj})
+        case 'ref'
+            ref = varargin{jj+1};
+    end
+    end
 end
 
 if isempty(fishPos)
@@ -49,13 +60,24 @@ end
 midlineInds = cell(size(IM,3),1);
 
 imgInds = 1:size(IM,3);
+%# Getting outside arena points to minimize drawing of midline segments on them
+if ~isempty(ref)
+    disp('Getting extra-arena points..')
+    rp = randperm(length(imgInds));
+    inds = imgInds(rp(1:min(length(rp),100)));
+    IM_sub = IM(:,:,inds);
+    minInt = min(IM_sub(:));
+    ref = bfilter2(Standardize(ref),12,[6 0.5]);
+    extraArenaInds = ref <= 0.15;
+end
+
 for imgNum = imgInds;
     img = IM(:,:,imgNum);
+    img(extraArenaInds) = minInt;    
     
-    [lineInds_all,parentMap] = GetMLs(img,fishPos(imgNum,:),dTh,heights);
+    [lineMat, ~] = GetMLs(img,fishPos(imgNum,:),dTh,heights); 
     
-    midlineInds{imgNum}= GetBestLine(img,lineInds_all,parentMap);
-   
+    [midlineInds{imgNum},~] = GetBestLine(img,lineMat,heights);    
     
     PlotLineInds(img,fishPos(imgNum,:),midlineInds{imgNum},imgNum)
 end
@@ -95,28 +117,37 @@ rImg = conv2(rImg,ker,'same');
 
 if isempty(prevStartPt)
     farInds = 1:size(indMat,1);
-else
-    lastInds = indMat(:,end);
-    [rL,cL] = ind2sub(size(im),lastInds);
-    x = cL; y = rL;
+else   
+    [y2,x2] = ind2sub(size(im),indMat(:,end));
+    postVecs = [x2, y2] - repmat(startPt,length(x2),1);
+    postVecs = postVecs(:,1) + postVecs(:,2).*1i;     
     preVec = startPt-prevStartPt;
-    preVec = preVec(1) + preVec(2)*1i;
-    
-    postVecs = [x y] - repmat(startPt,length(x),1);
-    postVecs = postVecs(:,1) + postVecs(:,2)*1i;
-    dAngles = angle(repmat(preVec,length(x),1).*conj(postVecs)) *(180/pi);
-    farInds = find(abs(dAngles)<=130);
+    preVecs = repmat(preVec(1) + preVec(2)*1i,length(x2),1);    
+    dAngles = angle(preVecs.*conj(postVecs)) *(180/pi);
+    farInds = find(abs(dAngles)<=140);
 end
 
 
-%## Find lines that pass through fish
-Standardize = @(x)(x-min(x))/(max(x)-min(x));
+%## Finding candidate midlines
 nml = rImg2nml(rImg);
-nml = nml(farInds);
-nml = Standardize(nml);
+thr = 0.1;
+maxtab(:,1) = GetPks(nml,'peakThr',thr,'thrType','abs','polarity',1);
+maxtab(:,2) = nml(maxtab(:,1));
 
-thr = 0.4;
-[maxtab,~] = peakdet(nml,thr);
+%## This next bit of code finds peaks that may not have been detected
+%## because 'rImg' is linear when it should have been circular
+midPt = round(length(nml)/2);
+pks_shift = GetPks(circshift(nml(:),midPt),'peakThr',thr,'thrType','abs','polarity',1);
+pks_bool = zeros(length(nml),1);
+pks_bool(pks_shift) = 1;
+pks_bool = circshift(pks_bool(:), -midPt);
+pks_shift = find(pks_bool);
+pks_shift = union(maxtab(:,1),pks_shift);
+maxtab=[];
+maxtab(:,1) = pks_shift;
+maxtab(:,2) = nml(maxtab(:,1));
+%##########
+
 count = 0;
 while (isempty(maxtab)) && (count <10)
     thr = thr*0.9;
@@ -124,26 +155,31 @@ while (isempty(maxtab)) && (count <10)
     disp('Lowering threshold to find segment...')
     count = count + 1;
 end
-if isempty(maxtab)
-    maxtab(:,1) = find(nml > thr);
-end
-if isempty(maxtab)
-    maxtab(:,1) = max(nml);
-end
 
-nPts = round((4/dTh));
+if isempty(maxtab)
+    [maxtab(:,2), maxtab(:,1)] = max(nml);
+end
+remInds = find(maxtab(:,2) < 0.4*max(maxtab(:,2)));
+maxtab(remInds,:)=[];
+
+[~,comInds] = intersect(maxtab(:,1),farInds);
+if ~isempty(comInds)
+    maxtab = maxtab(comInds,:);
+end
+nPts = round((2/dTh));
 probInds = GetPeriPts(maxtab(:,1),nPts);
 probInds(probInds<0) = 1;
 probInds(probInds > length(nml))= length(nml);
-probInds(probInds==0)=[];
-
-if isempty(probInds)
-    blahInds = farInds; % At the moment, not really dealing with a segment not being found!    
+probInds(probInds==0)=1;
+probInds = unique(probInds);
+keepInds = intersect(probInds,farInds);
+if isempty(keepInds)
+    blahInds = farInds; % At the moment, not really dealing with a segment not being found!
+    nml = nml(farInds);
 else
-    blahInds = farInds(probInds);
-    nml = nml(probInds);
+    blahInds = keepInds;
+    nml = nml(keepInds);
 end
-
 
 %## Find lines that are not contiguous blocks (i.e. islands) and eliminate
 [blockSizes,blockInds] = GetContiguousBlocks(blahInds);
@@ -155,10 +191,10 @@ blockEndInds = blockInds + blockSizes - 1;
 
 comInds = nan(size(blockSizes));
 for blk = 1:numel(blockSizes)
-    blkInds = blockInds(blk):blockEndInds(blk);  
+    blkInds = blockInds(blk):blockEndInds(blk);
     if sum(nml(blkInds))~=0
         comInds(blk) = blahInds(round(sum(blkInds(:).*nml(blkInds))/sum(nml(blkInds))));
-    end 
+    end
 end
 comInds(isnan(comInds))=[];
 lineInds = indMat(comInds,:)';
@@ -191,21 +227,68 @@ lineInds = indMat(comInds,:)';
 
 end
 
+
+function [lineMat,parentMap] = GetMLs(im,fishPos,dTh,lineLens)
+startPt = fishPos;
+prevStartPt  =[];
+lineInds = cell(numel(lineLens),1);
+parentMap = lineInds;
+lineInds_first = GetML(im,startPt,prevStartPt, dTh, lineLens(1));
+% blah = im/2; % For debugging only
+% blah(lineInds_first) = max(im(:)); % For debugging only
+for kk = 1:size(lineInds_first,2);
+    parentMap{1}{kk} = num2str(kk);
+    lineInds{1}{kk} = lineInds_first(:,kk);
+end
+for seg = 2:numel(lineLens)     
+    count = 0;
+    for ln = 1:length(lineInds{seg-1})
+        lInds = lineInds{seg-1}{ln};        
+        startPt = IndToSub(im,lInds(end));
+        prevStartPt = IndToSub(im,lInds(length(lInds)-lineLens(seg-1)+1));
+%         prevStartPt = IndToSub(im,lInds_seg(1));
+        temp = GetML(im,startPt,prevStartPt,dTh,lineLens(seg));
+        if isempty(temp)
+             temp = GetML(im,startPt,prevStartPt,dTh,lineLens(seg));
+        end
+        for kk = 1:size(temp,2)
+            count = count + 1;
+            parentMap{seg}{count} = [parentMap{seg-1}{ln} '_' num2str(kk)];
+            lineInds{seg}{count} = [lineInds{seg-1}{ln}; temp(:,kk)];
+%             blah = im/2; % For debugging only
+%             blah(lineInds{seg}{count}) = max(im(:)); % For debugging only           
+        end
+    end
+    blah = cell2mat(lineInds{seg});
+    if size(blah,2)>1        
+         bestLine = GetBestLine_sub(im,blah);        
+    else
+        bestLine = blah;
+    end
+    lineInds{seg} = {bestLine};
+end
+lineMat = cell2mat(lineInds{end});
+
+    function sub  = IndToSub(im,ind)
+        [r,c] = ind2sub(size(im),ind);
+        x = c; y = r;
+        sub = [x,y];
+    end
+end
+
 function nml = rImg2nml(rImg)
 Standardize = @(x)(x-min(x))/(max(x)-min(x));
 muPxls = mean(rImg,2);
 rImg2 = sort(rImg,2,'descend');
-temp = abs(rImg2-repmat(mean(rImg2,2)*0.9,1,size(rImg2,2)));
+% temp = abs(rImg2-repmat(mean(rImg2,2)*0.9,1,size(rImg2,2)));
+temp = abs(rImg2-repmat(median(rImg2,2)*0.9,1,size(rImg2,2)));
 [~, comInds] = min(temp,[],2);
-[lps,~] = GetLineProfileSpread(rImg);
-if sum(lps)==0 && sum(comInds)~=0
-     nml = Standardize(muPxls(:)).*Standardize(comInds(:)); 
-elseif sum(lps)==0 && sum(comInds)==0
-    nml = Standardize(muPxls(:));
-else   
-    nml = Standardize(muPxls(:)).*Standardize(lps(:)).*Standardize(comInds(:));
-end
-
+ker = gausswin(max(1,round(size(rImg,1)/30)));
+nml =  (Standardize(muPxls(:))).*comInds(:); % Standardization before multiplication is important so as no to convert valleys with negative values into peaks
+N = length(nml);
+nml = [nml(:); nml(:); nml(:)];
+nml = Standardize(conv2(nml, ker(:),'same'));
+nml = nml(N+1:2*N);
 end
 
 function PlotLineInds(img,fishPos,lineInds,imgNum)
@@ -220,83 +303,38 @@ hold on
 plot(fishPos(1),fishPos(2),'ko')
 title(num2str(imgNum))
 shg
+% pause()
 end
 
-function lineInds_best = GetBestLine(img,lineInds_all,parentMap)
-heights = nan(length(lineInds_all),1);
-for seg = 1:size(heights,1)
-    heights(seg) = size(lineInds_all{seg},1);
-end
-lineInds = nan(sum(heights),length(parentMap{end}));
-for ln = 1:size(lineInds,2)
-    strInd = parentMap{end}(ln);
-    blah = [];
-    for seg = 1:length(parentMap)        
-            segInd = str2num(strInd{1}(seg));
-            if segInd ==0
-                segInd = 1; % This is a hack for now! Need to replace with proper implementation later (20160523)
-            end
-            blah  = [blah; lineInds_all{seg}(:,segInd)];      
-    end
-    lineInds(:,ln) = blah;
+function lineMat = GetBestLine_sub(img,lineMat)
+lineProfiles = img(lineMat);
+nml = rImg2nml(lineProfiles');
+[~,ind] = max(nml);
+ind = ind(1);
+lineMat = lineMat(:,ind);
 end
 
-lineProfiles = img(lineInds);
+function [midlines, lineMat] = GetBestLine(img,lineMat,heights)
+lineProfiles = img(lineMat);
 % muPxls = mean(lineProfiles,1);
 % [lps,gof] = GetLineProfileSpread(lineProfiles');
 % lps = lps';
 % nml = muPxls.*lps.*gof;
+
 nml = rImg2nml(lineProfiles');
 [~,ind] = max(nml);
 ind = ind(1);
-lineInds = lineInds(:,ind);
-lineInds_best = cell(length(heights),1);
-lineInds_best{1} = lineInds(1:heights(1));
+lineMat = lineMat(:,ind);
+midlines = cell(length(heights),1);
+midlines{1} = lineMat(1:heights(1));
 startInd = 1;
 for seg = 2:numel(heights)
     startInd = startInd + heights(seg-1);
     segInds = startInd:+ startInd + heights(seg)-1;
-    lineInds_best{seg} = lineInds(segInds);
+    midlines{seg} = lineMat(segInds);
 end
 end
 
-function [lineInds_all,parentMap] = GetMLs(im,fishPos,dTh,lineLens)
-startPt = fishPos;
-prevStartPt  =[];
-lineInds_all = cell(numel(lineLens),1);
-numMap = lineInds_all;
-parentMap = numMap;
-lineInds_all{1} = GetML(im,startPt,prevStartPt, dTh, lineLens(1));
-numMap{1} = size(lineInds_all{1},2);
-for kk = 1:size(lineInds_all{1},2);
-    parentMap{1}{kk} = num2str(kk);
-end
-for seg = 2:numel(lineLens)
-    blah = lineInds_all{seg-1};
-    lineInds_sub =[];
-    numMap{seg}=[];
-    count = 0;
-    for ln = 1:size(blah,2)
-        lInds = blah(:,ln);
-        startPt = IndToSub(im,lInds(end));
-        prevStartPt = IndToSub(im,lInds(1));
-        temp = GetML(im,startPt,prevStartPt,dTh,lineLens(seg));
-        for kk = 1:size(temp,2)
-            count = count + 1;
-            parentMap{seg}{count} = [parentMap{seg-1}{ln} num2str(kk)];
-        end
-        numMap{seg} = [numMap{seg}, size(temp,2)];
-        lineInds_sub = [lineInds_sub, temp];
-    end
-    numMap{seg}  = cumsum(numMap{seg});
-    lineInds_all{seg} = lineInds_sub;
-end
-    function sub  = IndToSub(im,ind)
-        [r,c] = ind2sub(size(im),ind);
-        x = c; y = r;
-        sub = [x,y];
-    end
-end
 
 function [lps,gof] = GetLineProfileSpread(rImg)
 Y = sort(rImg,2,'ascend');
